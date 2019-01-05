f4f6cd3ed3dc35fd5b74fbbc7aed9ffaebea37d2
Jorge Ortiz <jortiz81@gmail.com>
2019-01-05 10:05:41 -0400

    testing markdown_generated scripts, removed citation and excerpt

---
+++ _publications/2009-10-01-paper-title-number-1.md
@@ -0,0 +1,10 @@
+---
+title: "Paper Title Number 1"
+collection: publications
+permalink: /publication/2009-10-01-paper-title-number-1
+date: 2009-10-01
+venue: 'Journal 1'
+paperurl: 'http://academicpages.github.io/files/paper1.pdf'
+---
+
+<a href='http://academicpages.github.io/files/paper1.pdf'>Download paper here</a>
--- markdown_generator/publications.py
+++ markdown_generator/publications.py
@@ -76,8 +76,8 @@ for row, item in publications.iterrows():
     
     md += """\npermalink: /publication/""" + html_filename
     
-    if len(str(item.excerpt)) > 5:
-        md += "\nexcerpt: '" + html_escape(item.excerpt) + "'"
+    #if len(str(item.excerpt)) > 5:
+    #   md += "\nexcerpt: '" + html_escape(item.excerpt) + "'"
     
     md += "\ndate: " + str(item.pub_date) 
     
@@ -86,7 +86,7 @@ for row, item in publications.iterrows():
     if len(str(item.paper_url)) > 5:
         md += "\npaperurl: '" + item.paper_url + "'"
     
-    md += "\ncitation: '" + html_escape(item.citation) + "'"
+    #md += "\ncitation: '" + html_escape(item.citation) + "'"
     
     md += "\n---"
     
@@ -95,10 +95,10 @@ for row, item in publications.iterrows():
     if len(str(item.paper_url)) > 5:
         md += "\n\n<a href='" + item.paper_url + "'>Download paper here</a>\n" 
         
-    if len(str(item.excerpt)) > 5:
-        md += "\n" + html_escape(item.excerpt) + "\n"
+    #if len(str(item.excerpt)) > 5:
+    #    md += "\n" + html_escape(item.excerpt) + "\n"
         
-    md += "\nRecommended citation: " + item.citation
+    #md += "\nRecommended citation: " + item.citation
     
     md_filename = os.path.basename(md_filename)
        
